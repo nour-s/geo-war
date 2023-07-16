a6c989f3d53d648520bef653c04e85063063cce8
Nour Sabouny <n.sabouny@gmail.com>
2023-07-16 16:53:19 +0400

    getting rid of the bullet rotation calculation as it is now set by the player correctly

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     // A bullet is an object that move fast forward the moment it gets initialized
-    public float speed = 1f;
+    public float speed = 10f;
     public float lifeTime = 1f;
     public Vector3 direction = Vector3.zero;
 
@@ -15,23 +15,12 @@ public class Bullet : MonoBehaviour
         Destroy(gameObject, lifeTime);
     }
 
-    /// Shoot is a  function that takes a direction and then moves the bullet in that direction
-    public void Shoot(Vector3 direction)
-    {
-        //rotate the bullet towards the direction
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
-
-        // set the direction of the bullet
-        this.direction = direction;
-        transform.rotation = Quaternion.AngleAxis(angle + 90, Vector3.forward);
-    }
 
     // Update is called once per frame
     void Update()
     {
         // Move the bullet in the direction using transform
-        // transform.Translate(direction.normalized * speed * Time.deltaTime);
-        transform.position += direction.normalized * speed * Time.deltaTime;
+        transform.position += transform.up * speed * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
