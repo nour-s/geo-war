c1a01e3a17fd7c026a5e5122ce322e9da581a5b5
Nour Sabouny <>
2023-07-23 22:59:30 +0400

    create hitable component

---
+++ Assets/Scripts/Hitable.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitable : MonoBehaviour
+{
+    public float health = 100f;
+
+    public float damage = 20f;
+
+    public SpriteRenderer spriteRenderer;
+
+    public void TakeDamage()
+    {
+        FlashEffect();
+        health -= damage;
+
+        StartCoroutine(FlashEffect());
+
+        if (health <= 0)
+        {
+            // Destroy the enemy
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator FlashEffect()
+    {
+        spriteRenderer.enabled = true;
+        yield return new WaitForSeconds(0.01f);
+        spriteRenderer.enabled = false;
+    }
+}
